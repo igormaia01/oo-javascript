874c3dc6c47390a3883a0b4155e9eb8c59bafcd6
Igor de Paulo Maia <igor.maia@syngenta.com>
2023-06-23 09:39:03 -0500

    objetos estaticos

---
+++ src/static/aluno.js
@@ -0,0 +1,21 @@
+export const alunosObject = [
+  {
+    nome: "Igor",
+    cpf: "333.333.333-33",
+    matricula: "123456",
+  },
+  {
+    nome: "Maria",
+    cpf: "444.444.444-44",
+    matricula: "654321",
+  },
+  {
+    nome: "José",
+    cpf: "555.555.555-55",
+    matricula: "987654",
+  },{
+    nome: "Ana",
+    cpf: "666.666.666-66",
+    matricula: "456789",
+  }
+]
